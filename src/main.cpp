31cc53de7716b9cd0e39a478bb0b6c8dcae70bd7
Minijackson <minijackson@riseup.net>
2017-12-01 11:50:50 -0500

    Add basic proof of concept

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,82 @@
-#include <iostream>
+#include "opencl.hpp"
+
+#include "dilation.hpp"
+#include "edge_ratio.hpp"
+#include "sobel.hpp"
+#include "threshold.hpp"
+
 #include <opencv2/opencv.hpp>
 
-#include "sobel.hpp"
+#include <iostream>
 
-int main(int argc, char const* argv[]) {
+constexpr const double LIMIT    = 0.6;
+constexpr const double FX_LIMIT = 0.8;
 
-	try {
-		const std::vector<std::pair<std::string, int>> vImages = {
-		        {"../data/airplane.png", cv::IMREAD_COLOR},
-		        {"../data/baboon.png", cv::IMREAD_COLOR},
-		        {"../data/cameraman.tif", cv::IMREAD_COLOR},
-		        {"../data/lena.png", cv::IMREAD_COLOR},
-		        {"../data/logo.tif", cv::IMREAD_COLOR},
-		        {"../data/logo_noise.tif", cv::IMREAD_COLOR},
-		        {"../data/peppers.png", cv::IMREAD_COLOR},
-		};
+constexpr const int PROGRESS_BAR_SIZE = 42;
 
-		for(const std::pair<std::string, int>& oImagePathFlag : vImages) {
-			const cv::Mat_<cv::Vec3b> oInput =
-			        cv::imread(oImagePathFlag.first, oImagePathFlag.second);
-			if(oInput.empty()) {
-				std::cerr << "Could not load image at '" << oImagePathFlag.first << "'; exiting..."
-				          << std::endl;
-				std::exit(-1);
-			}
-			std::cout << "\nProcessing image at '" << oImagePathFlag.first << "'..." << std::endl;
+cv::Mat_<uint8_t> pipeline(cv::Mat_<cv::Vec4b> input) {
+	const auto sobel_res  = sobel_cl(input);
+	const auto thresh_res = threshold_gnupar(sobel_res.get(), 170);
 
-			auto result = sobel(oInput);
-			cv::imshow("Sobel result", result);
+	return thresh_res;
+}
 
-			cv::waitKey(0);
+int main() {
+	cl_singletons::setup();
+
+	std::cout << "Loading video..." << std::endl;
+	cv::VideoCapture input_video("../data/INF4710_TP3_A2017_video.avi");
+
+	size_t frame_count = input_video.get(CV_CAP_PROP_FRAME_COUNT), frame_id = 1;
+
+	std::cout << "Processing " << frame_count << " frames\n\n";
+
+	cv::Mat first_frame, second_frame;
+	cv::Mat_<cv::Vec4b> first_frame4, second_frame4;
+
+	input_video >> first_frame;
+	input_video >> second_frame;
+
+	std::vector<cv::Mat_<cv::Vec3b>> scenes{first_frame.clone()};
+
+	cv::cvtColor(first_frame, first_frame4, CV_RGB2RGBA);
+
+	cv::Mat_<uint8_t> first_thresh_res = pipeline(first_frame4);
+
+	while(!second_frame.empty()) {
+		if(frame_id % 50 == 0) {
+			size_t progress = (frame_id / (double)frame_count) * PROGRESS_BAR_SIZE;
+			std::cout << "[1000D[" << std::string(progress, '#')
+			          << std::string(PROGRESS_BAR_SIZE - progress, ' ') << "]" << std::flush;
 		}
-	} catch(...) {
-		std::cerr << "Caught unhandled exception." << std::endl;
+		++frame_id;
+
+		cv::cvtColor(second_frame, second_frame4, CV_RGB2RGBA);
+
+		cv::Mat_<uint8_t> second_thresh_res = pipeline(second_frame4);
+
+		const auto first_dilation_res = dilate_cl(first_thresh_res, 3);
+
+		const double ratio = edge_ratio_omp(first_dilation_res.get(), second_thresh_res);
+
+		if(ratio < LIMIT) {
+			// std::cout << "Ratio = " << ratio << ", pushing_back\n";
+			// cv::imshow("Dilated", first_dilation_res.get());
+			// cv::imshow("Next treshed", second_thresh_res);
+			// cv::waitKey(0);
+			scenes.push_back(second_frame.clone());
+		} else if(ratio < FX_LIMIT) {
+			std::cout << "TODO\n";
+		}
+
+		first_thresh_res = second_thresh_res;
+		input_video >> second_frame;
+	}
+	std::cout << "[1000D[" << std::string(PROGRESS_BAR_SIZE, '#') << "]" << std::flush;
+	std::cout << "\n" << std::endl;
+
+	for(auto const& scene : scenes) {
+		cv::imshow("Scene", scene);
+		cv::waitKey(0);
 	}
 }
