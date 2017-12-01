f4da01923f5f0ee176dae0544c9b2f51d0c658f5
Minijackson <minijackson@riseup.net>
2017-11-30 19:45:25 -0500

    Add sobel benchmark

--- benchmarks/sobel.cpp
+++ benchmarks/sobel.cpp
@@ -1,11 +1,32 @@
 #include "common.hpp"
 
+#include "../src/sobel.hpp"
+
 #include <hayai.hpp>
 
-//BENCHMARK_F(SmallRandomImageFixture, nothingInteresting, 10, 100) {
-	//std::cerr << image << std::endl;
-//}
+#define SOBEL_BASE_BENCHMARK(fixture)                                                              \
+	BENCHMARK_F(fixture, mono_threaded, 10, 100) {                                                 \
+		sobel(image);                                                                              \
+	}
 
-//BENCHMARK_F(BigRandomImageFixture, nothingInteresting, 10, 100) {
-	//std::cerr << image << std::endl;
-//}
+#define SOBEL_OPENCL_BENCHMARK(fixture)                                                            \
+	BENCHMARK_F(fixture, opencl, 10, 100) {                                                        \
+		sobel_cl(image4);                                                                          \
+	}
+
+#define SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(fixture)                                              \
+	SOBEL_BASE_BENCHMARK(fixture)                                                                  \
+	SOBEL_OPENCL_BENCHMARK(fixture)
+
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(EDTV480RandomImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(EDTV576RandomImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(HDTV720RandomImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(HDTV1080RandomImageFixture);
+
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(AirplaneFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(BaboonFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(CameramanFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(LenaFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(LogoNoiseFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(LogoFixedImageFixture);
+SOBEL_ALL_IMPLEMENTATIONS_BENCHMARKS(PeppersFixedImageFixture);
